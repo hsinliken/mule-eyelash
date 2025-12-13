dd24314173fbb9b6a136d67bd4abb7f3fd5cd8f6
kenlee <kenlee@kenleedeMacBook-Pro.local>
2025-12-13 08:07:28 +0800

    Update LIFF ID

--- contexts/ShopContext.tsx
+++ contexts/ShopContext.tsx
@@ -12,9 +12,9 @@ const ShopContext = createContext<ShopContextType>({
     subtitle: 'STUDIO',
     logo: '',
     lineId: '@0970421267',
-    liffId: '2008673814-tF4zCZOH'
+    liffId: '2008685421-wjbyqf1k'
   },
-  updateSettings: () => {},
+  updateSettings: () => { },
 });
 
 export const useShop = () => useContext(ShopContext);
@@ -25,7 +25,7 @@ export const ShopProvider: React.FC<{ children: React.ReactNode }> = ({ children
     subtitle: 'STUDIO',
     logo: 'https://cdn-icons-png.flaticon.com/512/3163/3163219.png', // 預設暫時圖示
     lineId: '@0970421267', // 預設商家 LINE ID
-    liffId: '2008673814-tF4zCZOH' // 預設 LIFF ID
+    liffId: '2008685421-wjbyqf1k' // 預設 LIFF ID
   });
 
   const updateSettings = (newSettings: Partial<ShopSettings>) => {
