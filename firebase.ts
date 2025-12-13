9434a0be165d7e1de7369c382673c06b3590612d
kenlee <kenlee@kenleedeMacBook-Pro.local>
2025-12-13 13:23:00 +0800

    Fix: update firebase config with new project credentials

--- firebase.ts
+++ firebase.ts
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDcNR1yJmtx9SnT3VPNsQL4IPG6th7O3iE",
-    authDomain: "mule-eyelash.firebaseapp.com",
-    projectId: "mule-eyelash",
-    storageBucket: "mule-eyelash.firebasestorage.app",
-    messagingSenderId: "841390413072",
-    appId: "1:841390413072:web:0dc9c8a9e9702b54d94195",
-    measurementId: "G-YJMGKJ1GKD"
+    apiKey: "AIzaSyAAzykzKArn_nqtQbROjMxJ1aRN_ZOS6B4",
+    authDomain: "mule-eyelash-45c26.firebaseapp.com",
+    projectId: "mule-eyelash-45c26",
+    storageBucket: "mule-eyelash-45c26.firebasestorage.app",
+    messagingSenderId: "767066119476",
+    appId: "1:767066119476:web:5f4503f762b82d7bdf5961",
+    measurementId: "G-H1B7SJCTPT"
 };
 
 const app = initializeApp(firebaseConfig);
